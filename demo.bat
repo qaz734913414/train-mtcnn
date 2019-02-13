018cb4a14b594687f67f99d0a10215137fcbd0b6
Zuo Qing <zuoqing1988@aliyun.com>
2019-02-13 09:08:14 +0800

    添加一个可以测试demo的模型

--- demo.bat
+++ demo.bat
@@ -1,1 +1,1 @@
-python demo20.py --prefix model/pnet20_v1,model/rnet_v1,model/onet_v3 --epoch 16,16,10 --imgfile 4.jpg
+python demo20.py --prefix model/pnet20_v1,model/rnet_v1,model/onet_v1 --epoch 16,16,16 --imgfile 4.jpg
