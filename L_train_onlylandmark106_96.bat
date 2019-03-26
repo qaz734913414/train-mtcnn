f0566eacfd459a70fa902c305a18f5644ee41425
Zuo Qing <zuoqing1988@aliyun.com>
2019-03-26 13:42:03 +0800

    更改106训练数据为106data_merge

--- L_train_onlylandmark106_96.bat
+++ L_train_onlylandmark106_96.bat
@@ -1,3 +1,3 @@
 set MXNET_CUDNN_AUTOTUNE_DEFAULT=0
-python example\train_L_net_onlylandmark106_96.py --lr 0.01 --image_set landmark106 --end_epoch 5000 --lr_epoch 1500,2500,8000 --batch_size 100 --thread_num 10 --frequent 10 
+python example\train_L_net_onlylandmark106_96.py --lr 0.01 --image_set 106data_merge --end_epoch 8000 --lr_epoch 2500,5000,8000 --batch_size 100 --thread_num 10 --frequent 10 
 pause 
