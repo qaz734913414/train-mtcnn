9a343f4939ae8624002906ea6962ad33cd7d988b
Zuo Qing <zuoqing1988@aliyun.com>
2019-04-02 08:56:51 +0800

    更新脚本

--- L_train_onlylandmark_64.bat
+++ L_train_onlylandmark_64.bat
@@ -1,3 +1,3 @@
 set MXNET_CUDNN_AUTOTUNE_DEFAULT=0
-python example\train_L_net_onlylandmark_64.py --lr 0.01 --image_set img_cut_celeba_all --end_epoch 5000 --prefix model/lnet --lr_epoch 2000,4000 --batch_size 100 --thread_num 10 --frequent 100 
+python example\train_L_net_onlylandmark_64.py --lr 0.01 --image_set img_cut_celeba_all --end_epoch 5000 --pretrained model/lnet64_v3 --prefix model/lnet64_v3 --lr_epoch 2000,4000 --batch_size 100 --thread_num 10 --frequent 100 
 pause 
