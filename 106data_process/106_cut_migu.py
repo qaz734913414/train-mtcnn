fa94afa0037d75dc28a96a429dfcc142a1e75329
Zuo Qing <zuoqing1988@aliyun.com>
2019-04-19 10:58:17 +0800

    添加106点数据转换脚本

---
+++ 106data_process/106_cut_migu.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+
+
+def get_bbox(landmark):
+    x_coords = landmark[0::2]
+    y_coords = landmark[1::2]
+    x1 = min(x_coords)
+    x2 = max(x_coords)
+    y1 = min(y_coords)
+    y2 = max(y_coords)
+    return x1,y1, x2-x1, y2-y1
+	
+img_path = '../data_cut/'
+out_img_path = '../data_cut_migu/'
+
+with open('../cut_landmarks.txt', 'r') as f:
+    landmark_lines = f.readlines()
+f = open('../cut_landmarks_migu.txt','w')
+num = len(landmark_lines)
+for i in range(num):
+    line = landmark_lines[i].split('\n')[0]
+    landmark_splits = line.split()
+    img = cv2.imread(img_path+'/'+landmark_splits[0])
+    width = img.shape[1]
+    height = img.shape[0]
+    landmark = np.array(landmark_splits[1:213],dtype=np.float32)
+    x1,y1,w,h = get_bbox(landmark)
+    #print([x1,y1,w,h])
+    off_x = int(max(0,x1-1.5*w))
+    off_y = int(max(0,y1-1.5*h))
+    max_x = int(min(width,x1+w*2.5))
+    max_y = int(min(height,y1+h*2.5))
+    bbox = np.empty([4],dtype=np.float32)
+    bbox_ori = [x1,y1,w,h]
+    bbox[0] = max(0,x1)
+    bbox[1] = max(0,y1)	
+    bbox[2] = x1+w - bbox[0]
+    bbox[3] = y1+h - bbox[1]
+    bbox_ori[0] -= off_x
+    bbox_ori[1] -= off_y
+    for j in range(106):
+        landmark[j*2] -= off_x
+        landmark[j*2+1] -= off_y
+    if bbox[2] >= 20 and bbox[3] >= 20 and bbox[0]+bbox[2] <= width and bbox[1]+bbox[3] <= height:
+        cut_width = max_x-off_x
+        cut_height = max_y-off_y
+        cut_img = img[off_y:max_y,off_x:max_x,:]
+        if cut_width > 300 and cut_height > 300:
+            scale = 300.0/min(cut_width,cut_height)
+            dst_width = int(cut_width*scale)
+            dst_height = int(cut_height*scale)
+            cut_img = cv2.resize(cut_img,(dst_width,dst_height),interpolation=cv2.INTER_LINEAR)
+            bbox_ori *= scale
+            landmark *= scale
+        out_img_name = 'face%05d_%s'%(i,landmark_splits[0])
+        cv2.imwrite(out_img_path+'/'+out_img_name,cut_img)
+        line = out_img_name+' '
+        for idx in range(106):
+            line = line + '%.1f %.1f '%(landmark[idx*2],landmark[idx*2+1])
+        line = line + '\n'
+        f.write(line)
+    if (i+1)%100 == 0:
+        print i+1
+	
+f.close()
---
+++ 106data_process/vis.py
@@ -0,0 +1,45 @@
+"""
+Image visualization result for 106 landmarks
+Created by Jacky LUO
+Usage: python vis.py 
+"""
+import os
+import cv2
+import numpy as np
+
+base_path = '../data/'
+scale = int(5)
+if __name__ == '__main__':
+    with open("../box_landmark.txt") as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip().split()
+            # image_path, box, 106-landmarks
+            image_path = line[0]
+            b_box = line[1: 5]
+            landmarks = line[5:]
+
+            # visualization
+            img = cv2.imread(os.path.join(base_path, image_path))
+            h = img.shape[0]
+            w = img.shape[1]
+            img =cv2.resize(img,(w*scale,h*scale))
+            print scale
+            off_x = int(b_box[2])*scale
+            off_y = int(b_box[1])*scale
+            rect_h = int(b_box[0])*scale
+            rect_w = int(b_box[3])*scale
+            print off_x,off_y,rect_w,rect_h
+            cv2.rectangle(img, (int(off_x), int(off_y)),
+                          (int(off_x+rect_w), int(off_y+rect_h)), (0, 0, 255), 2)
+            landmarks = np.reshape(landmarks, (-1, 2))
+            idx = 0
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            for (x, y) in landmarks:
+                cv2.circle(img, (int(x)*scale, int(y)*scale), 2, (255, 0, 0), -1)
+                cv2.putText(img, '%d'%idx, (int(x)*scale, int(y)*scale), font, 0.8, (0, 255, 0), 2)
+                idx = idx+1
+            # cv2.imwrite("image.png", img)
+            cv2.imshow("image", img)
+            cv2.imwrite('save.jpg',img)
+            cv2.waitKey(0)
