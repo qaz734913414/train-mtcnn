f3f5bd401f592b5d0e993e9b3620528e0a3cfeab
Zuo Qing <zuoqing1988@aliyun.com>
2019-06-10 08:29:15 +0800

    更改脚本

--- tools/image_processing.py
+++ tools/image_processing.py
@@ -17,66 +17,41 @@ def transform(im, train = False):
     if train:
         scale = np.random.randint(3,7) * 0.2
         im = im * scale
-        if config.enable_gray:
+        if config.enable_gray or config.enable_color_jitter:
             gray_flag = np.random.randint(0,2)
-            if gray_flag == 1:
+            if gray_flag == 1 and config.enable_color_jitter:
                 #im[:,:,:] = np.sum(im * [0.114,0.587,0.299], axis=-1, keepdims=True)
                 gray_im = im[:,:,0]*0.114+im[:,:,1]*0.587+im[:,:,2]*0.299
                 im[:,:,0] = gray_im
                 im[:,:,1] = gray_im
                 im[:,:,2] = gray_im
-                
+            else:
+                scale_0 = 1.0+0.001*np.random.randint(-200,201)
+                scale_1 = 1.0+0.001*np.random.randint(-200,201)
+                scale_2 = 1.0+0.001*np.random.randint(-200,201)
+                im[:,:,0] *= scale_0
+                im[:,:,1] *= scale_1
+                im[:,:,2] *= scale_2
+                im = np.clip(im,0.0,255.0)
+			
+        if config.enable_noise:
+            mean = 0
+            var = np.random.randint(0,10)+1.0
+            im = gaussian_noise(im,mean,var)
+            
 		
     im_tensor = im.transpose(2, 0, 1)
     im_tensor = im_tensor[np.newaxis, :]
     im_tensor = (im_tensor - 127.5)*0.0078125
     return im_tensor
 
-def SaltAndPepper(src,percetage):  
-    SP_NoiseImg=src.copy()
-    SP_NoiseNum=int(percetage*src.shape[0]*src.shape[1]) 
-    for i in range(SP_NoiseNum): 
-        randR=np.random.randint(0,src.shape[0]-1) 
-        randG=np.random.randint(0,src.shape[1]-1) 
-        randB=np.random.randint(0,3)
-        if np.random.randint(0,1)==0: 
-            SP_NoiseImg[randR,randG,randB]=0 
-        else: 
-            SP_NoiseImg[randR,randG,randB]=255 
-    return SP_NoiseImg 
-def addGaussianNoise(image,percetage): 
-    G_Noiseimg = image.copy()
-    w = image.shape[1]
-    h = image.shape[0]
-    G_NoiseNum=int(percetage*image.shape[0]*image.shape[1]) 
-    for i in range(G_NoiseNum): 
-        temp_x = np.random.randint(0,h) 
-        temp_y = np.random.randint(0,w) 
-        G_Noiseimg[temp_x][temp_y][np.random.randint(3)] = np.random.randn(1)[0] 
-    return G_Noiseimg
-#dimming
-def darker(image,percetage=0.9):
-    image_copy = image.copy()
-    w = image.shape[1]
-    h = image.shape[0]
-    #get darker
-    for xi in range(0,w):
-        for xj in range(0,h):
-            image_copy[xj,xi,0] = int(image[xj,xi,0]*percetage)
-            image_copy[xj,xi,1] = int(image[xj,xi,1]*percetage)
-            image_copy[xj,xi,2] = int(image[xj,xi,2]*percetage)
-    return image_copy
-def brighter(image, percetage=1.5):
-    image_copy = image.copy()
-    w = image.shape[1]
-    h = image.shape[0]
-    #get brighter
-    for xi in range(0,w):
-        for xj in range(0,h):
-            image_copy[xj,xi,0] = np.clip(int(image[xj,xi,0]*percetage),a_max=255,a_min=0)
-            image_copy[xj,xi,1] = np.clip(int(image[xj,xi,1]*percetage),a_max=255,a_min=0)
-            image_copy[xj,xi,2] = np.clip(int(image[xj,xi,2]*percetage),a_max=255,a_min=0)
-    return image_copy
+def gaussian_noise(image, mean=0, var=0.001):
+    tmp_image = np.array(image, dtype=float)
+    noise = np.random.normal(mean, var ** 0.5, image.shape)
+    out = tmp_image + noise
+    out = np.clip(out, 0.0, 255.0)
+    return out
+
 
 def rotateWithLandmark(image, landmark, angle, scale):
     if angle == 0:
